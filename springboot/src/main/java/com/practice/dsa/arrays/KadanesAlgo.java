9498ce23caaab44bcf28455ac611f3010100dc62
Monishapleo <monishapleo@gmail.com>
2026-08-13 15:14:35 +0530

    kadane and prefix sum

---
+++ springboot/src/main/java/com/practice/dsa/arrays/KadanesAlgo.java
@@ -0,0 +1,19 @@
+package com.practice.dsa.arrays;
+
+public class KadanesAlgo {
+    public static void main(String[] args) {
+
+    }
+    static int maxSubArray(int[] nums){
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        for(int num : nums){
+            currentSum += num;
+            maxSum = Math.max(maxSum, currentSum);
+            if(currentSum < 0){
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
--- springboot/src/main/java/com/practice/dsa/arrays/PrefixSum.java
+++ springboot/src/main/java/com/practice/dsa/arrays/PrefixSum.java
@@ -4,8 +4,9 @@ import java.util.Arrays;
 
 public class PrefixSum {
     public static void main(String[] args) {
-        int[] arr={2,3,4,5};
-        System.out.println(Arrays.toString(buildPrefixSum(arr)));
+        int[] arr={1,7,3,6,5,6};
+        //System.out.println(Arrays.toString(buildPrefixSum(arr)));
+        System.out.println(pivotIndex(arr));
     }
     static int[] buildPrefixSum(int[] arr){
         int previousSum=0;
@@ -15,5 +16,27 @@ public class PrefixSum {
             previousSum=currentSum;
         }
         return arr;
+    }
+
+    static int rangeSum(int[] prefix, int left, int right) {
+        if (left == 0) {
+            return prefix[right];
+        }
+        return prefix[right] - prefix[left - 1];
+    }
+    static int pivotIndex(int[] nums){
+        int leftSum = 0;
+        int totalSum=0;
+        for(int num : nums){
+            totalSum+=num;
+        }
+        for(int i=0; i<nums.length;i++){
+            int rightSum = totalSum - leftSum - nums[i];
+            if(leftSum == rightSum){
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
     }
 }
